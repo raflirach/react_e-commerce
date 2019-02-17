071ab6abda22e104c3ebdc35458c9d2cc16f5c75
rafli <rafli060395@email.unikom.ac.id>
2019-02-17 13:08:18 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <React.Fragment>
         <Navbar />
         <Switch>
-          <Route path="/" component={ProductList} />
+          <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route component={Default} />
