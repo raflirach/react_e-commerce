e8c474a6f08554d3e364912a818570affdcc6844
rafli <rafli060395@email.unikom.ac.id>
2019-05-09 13:44:42 +0700

    Updates

---
+++ precache-manifest.0af277ee55a5277b37496fbb8c1abfec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react_e-commerce/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "a353e369736b221aa80a",
+    "url": "/react_e-commerce/static/js/runtime~main.a353e369.js"
+  },
+  {
+    "revision": "14faa70c6252f88bf0d2",
+    "url": "/react_e-commerce/static/js/main.14faa70c.chunk.js"
+  },
+  {
+    "revision": "df466eff480229638b6c",
+    "url": "/react_e-commerce/static/js/2.df466eff.chunk.js"
+  },
+  {
+    "revision": "14faa70c6252f88bf0d2",
+    "url": "/react_e-commerce/static/css/main.ffcdc652.chunk.css"
+  },
+  {
+    "revision": "df466eff480229638b6c",
+    "url": "/react_e-commerce/static/css/2.79651b4c.chunk.css"
+  },
+  {
+    "revision": "a32af1428571666621998a0236786d14",
+    "url": "/react_e-commerce/index.html"
+  }
+];
